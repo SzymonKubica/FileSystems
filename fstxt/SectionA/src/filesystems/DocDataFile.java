82bd6f7d9f1c921a09dcceb601c95be50c03203e
SzymonKubica <szymo.kubica@gmail.com>
2021-04-22 12:08:56 +0200

    The first part of Section A has been implemented and tested.

--- fstxt/SectionA/src/filesystems/DocDataFile.java
+++ fstxt/SectionA/src/filesystems/DocDataFile.java
@@ -1,5 +1,54 @@
 package filesystems;
 
-public class DocDataFile {
+public class DocDataFile extends DocFile {
+  private final byte[] contents;
 
+  /**
+   * Construct a file with the given name.
+   *
+   * @param name The name of the file.
+   */
+  public DocDataFile(String name, byte[] contents) {
+    super(name);
+    this.contents = contents;
+  }
+
+  public boolean containsByte(byte item) {
+    for (byte element : contents) {
+      if (element == item) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @Override
+  public int getSize() {
+    return getName().length() + contents.length;
+  }
+
+  @Override
+  public boolean isDirectory() {
+    return false;
+  }
+
+  @Override
+  public boolean isDataFile() {
+    return true;
+  }
+
+  @Override
+  public DocDirectory asDirectory() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public DocDataFile asDataFile() {
+    return this;
+  }
+
+  @Override
+  public DocFile duplicate() {
+    return new DocDataFile(getName(), contents);
+  }
 }
